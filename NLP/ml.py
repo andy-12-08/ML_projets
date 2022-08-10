230293e22c37edda48a1113e93ff7613d650be1c
andy-12-08 <aokafo3@lsu.edu>
2022-08-10 18:44:10 -0500

    changes

--- NLP/ml.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-from sklearn.linear_model import LinearRegression
-import pickle
-
-
-model.fit(df[['sepal_length','sepal_width']], df[['species']])
-pickle.dump(model, open('model.pkl', 'wb'))
